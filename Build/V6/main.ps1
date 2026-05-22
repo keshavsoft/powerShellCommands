8fc11298011e83bf343963eb74417c4356a0678a
keshavsoft <nknnkumar@yahoo.com>
2026-05-22 08:28:30 +0530

    v6 new git repo

---
+++ Build/V6/main.ps1
@@ -0,0 +1,31 @@
+$repos = @(
+    "https://github.com/keshavsoft/powerShellCommands",    
+    "https://github.com/keshavsoft/express-todo",
+    "https://github.com/keshavsoft/kschema-api-gen",
+    "https://github.com/keshavsoft/kschema-cli",
+    "https://github.com/keshavsoft/kschema",
+    "https://github.com/keshavsoft/kschema-api-validator",
+    "https://github.com/keshavsoft/kschema-cli-samples",
+    "https://github.com/keshavsoft/kschema-api-check",
+    "https://github.com/keshavsoft/kschema-api-gen-actions",
+    "https://github.com/keshavsoft/json-crud-ui-components",
+    "https://github.com/keshavsoft/tailwind-todo"    
+)
+
+$baseFolder = "D:\KeshavSoftRepos"
+
+$targetFolder = Get-WorkspaceFolder
+
+Ensure-BaseFolder -inBaseFolder $baseFolder
+
+Ensure-WorkspaceFolder -inTargetFolder $targetFolder
+
+if (-not (Ensure-GitInstalled)) {
+    return
+}
+
+Clone-Repos -inRepos $repos -inTargetFolder $targetFolder
+
+Install-NpmPackages -inTargetFolder $targetFolder
+
+Open-Workspace -inTargetFolder $targetFolder
