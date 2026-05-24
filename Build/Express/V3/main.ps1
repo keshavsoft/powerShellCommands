665704a78d9a5c8e5c0f3d36f436f20e31afc25e
keshavsoft <nknnkumar@yahoo.com>
2026-05-24 11:21:21 +0530

    repo add

---
+++ Build/Express/V3/main.ps1
@@ -0,0 +1,25 @@
+$repos = @(
+    "https://github.com/keshavsoft/vs-code-ext-boilerplate",    
+    "https://github.com/keshavsoft/vs-code-ext-express-api-gen",
+    "https://github.com/keshavsoft/express-todo",
+    "https://github.com/keshavsoft/kschema-api-gen-appjs",
+    "https://github.com/keshavsoft/kschema-api-gen-routesjs"
+)
+
+$baseFolder = "D:\KeshavSoftRepos"
+
+$targetFolder = Get-WorkspaceFolder
+
+Ensure-BaseFolder -inBaseFolder $baseFolder
+
+Ensure-WorkspaceFolder -inTargetFolder $targetFolder
+
+if (-not (Ensure-GitInstalled)) {
+    return
+}
+
+Clone-Repos -inRepos $repos -inTargetFolder $targetFolder
+
+Install-NpmPackages -inTargetFolder $targetFolder
+
+Open-Workspace -inTargetFolder $targetFolder
