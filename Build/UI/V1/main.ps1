4bc978bd78c21a4f80a19c8968f6d2171ee8f41f
keshavsoft <nknnkumar@yahoo.com>
2026-05-26 11:01:42 +0530

    UI V1

---
+++ Build/UI/V1/main.ps1
@@ -0,0 +1,30 @@
+$repos = @(
+    "https://github.com/keshavsoft/tailwind-todo",    
+    "https://github.com/keshavsoft/vs-code-ext-express-api-gen",
+    "https://github.com/keshavsoft/vs-code-ext-express-api-gen-routes",
+    "https://github.com/keshavsoft/vs-code-ext-express-api-gen-endpoints",
+    "https://github.com/keshavsoft/vs-code-ext-express-api-gen-actions",
+    "https://github.com/keshavsoft/express-todo",
+    "https://github.com/keshavsoft/kschema-api-gen-appjs",
+    "https://github.com/keshavsoft/kschema-api-gen-routesjs",
+    "https://github.com/keshavsoft/kschema-api-gen-endpointsjs",
+    "https://github.com/keshavsoft/kschema-api-gen-actions"
+)
+
+$baseFolder = "D:\KeshavSoftRepos"
+
+$targetFolder = Get-WorkspaceFolder
+
+Ensure-BaseFolder -inBaseFolder $baseFolder
+
+Ensure-WorkspaceFolder -inTargetFolder $targetFolder
+
+if (-not (Ensure-GitInstalled)) {
+    return
+}
+
+Clone-Repos -inRepos $repos -inTargetFolder $targetFolder
+
+Install-NpmPackages -inTargetFolder $targetFolder
+
+Open-Workspace -inTargetFolder $targetFolder
