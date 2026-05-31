7eecc01d165ae6d88528101e4f4de01c0aadd04b
keshavsoft <nknnkumar@yahoo.com>
2026-05-31 21:33:18 +0530

    KSchemaFs 1

---
+++ Build/KSchemaFs/V1/main.ps1
@@ -0,0 +1,23 @@
+$repos = @(
+    "https://github.com/keshavsoft/express-fix-endPoints-js",    
+    "https://github.com/keshavsoft/kschema-fs-api-gen-actions",
+    "https://github.com/keshavsoft/json-crud-fs"
+)
+
+$baseFolder = "D:\KeshavSoftRepos"
+
+$targetFolder = Get-WorkspaceFolder
+
+Ensure-BaseFolder -inBaseFolder $baseFolder
+
+Ensure-WorkspaceFolder -inTargetFolder $targetFolder
+
+if (-not (Ensure-GitInstalled)) {
+    return
+}
+
+Clone-Repos -inRepos $repos -inTargetFolder $targetFolder
+
+Install-NpmPackages -inTargetFolder $targetFolder
+
+Open-Workspace -inTargetFolder $targetFolder
