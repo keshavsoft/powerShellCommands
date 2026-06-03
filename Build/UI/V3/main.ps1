96a244db08ca8d8ecbfa53f11ee081abb1c93277
keshavsoft <nknnkumar@yahoo.com>
2026-06-03 09:07:36 +0530

    ui 3

---
+++ Build/UI/V3/main.ps1
@@ -0,0 +1,26 @@
+$repos = @(
+    "https://github.com/keshavsoft/tailwind-todo",    
+    "https://github.com/keshavsoft/tailwind-table-dom",    
+    "https://github.com/keshavsoft/tailwind-header",    
+    "https://github.com/keshavsoft/json-crud-ui-init",
+    "https://github.com/keshavsoft/vs-code-ext-json-crud-ui-init",
+    "https://github.com/keshavsoft/EndPointGenWebsite"
+)
+
+$baseFolder = "D:\KeshavSoftRepos"
+
+$targetFolder = Get-WorkspaceFolder
+
+Ensure-BaseFolder -inBaseFolder $baseFolder
+
+Ensure-WorkspaceFolder -inTargetFolder $targetFolder
+
+if (-not (Ensure-GitInstalled)) {
+    return
+}
+
+Clone-Repos -inRepos $repos -inTargetFolder $targetFolder
+
+Install-NpmPackages -inTargetFolder $targetFolder
+
+Open-Workspace -inTargetFolder $targetFolder
