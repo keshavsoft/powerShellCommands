eaa97eeae84fa8fd7d08a44b1aabbedbb1f4adce
keshavsoft <nknnkumar@yahoo.com>
2026-06-04 08:47:17 +0530

    acc v1

---
+++ Build/Accounts/V1/main.ps1
@@ -0,0 +1,25 @@
+$repos = @(
+    "https://github.com/keshavsoft/accountsV8",    
+    "https://github.com/keshavsoft/kschema-fs-api-gen-actions",
+    "https://github.com/keshavsoft/json-crud-fs",
+    "https://github.com/keshavsoft/json-crud-ui-table",
+    "https://github.com/keshavsoft/tailwind-table-dom"
+)
+
+$baseFolder = "D:\KeshavSoftRepos"
+
+$targetFolder = Get-WorkspaceFolder
+
+Ensure-BaseFolder -inBaseFolder $baseFolder
+
+Ensure-WorkspaceFolder -inTargetFolder $targetFolder
+
+if (-not (Ensure-GitInstalled)) {
+    return
+}
+
+Clone-Repos -inRepos $repos -inTargetFolder $targetFolder
+
+Install-NpmPackages -inTargetFolder $targetFolder
+
+Open-Workspace -inTargetFolder $targetFolder
