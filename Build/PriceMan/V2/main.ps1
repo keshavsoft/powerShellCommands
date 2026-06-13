0b4189e185579290f525efe763a613e9766db119
keshavsoft <nknnkumar@yahoo.com>
2026-06-13 12:13:52 +0530

    pr 2

---
+++ Build/PriceMan/V2/main.ps1
@@ -0,0 +1,31 @@
+$repos = @(
+    "https://github.com/keshavsoft/priceListV1",    
+    "https://github.com/keshavsoft/kschema-fs-api-gen-actions",
+    "https://github.com/keshavsoft/json-crud-fs",
+    "https://github.com/keshavsoft/json-crud-ui-table",
+    "https://github.com/keshavsoft/json-crud-ui-comp",
+    "https://github.com/keshavsoft/tailwind-table-dom",
+    "https://github.com/keshavsoft/tailwind-header-dom",
+    "https://github.com/keshavsoft/tailwind-table-dom-comp",
+    "https://github.com/keshavsoft/tailwind-vertical-dom",
+    "https://github.com/keshavsoft/tailwind-gen-css",
+    "https://github.com/keshavsoft/vs-code-ext-json-crud-ui-table"
+)
+
+$baseFolder = "D:\KeshavSoftRepos"
+
+$targetFolder = Get-WorkspaceFolder
+
+Ensure-BaseFolder -inBaseFolder $baseFolder
+
+Ensure-WorkspaceFolder -inTargetFolder $targetFolder
+
+if (-not (Ensure-GitInstalled)) {
+    return
+}
+
+Clone-Repos -inRepos $repos -inTargetFolder $targetFolder
+
+Install-NpmPackages -inTargetFolder $targetFolder
+
+Open-Workspace -inTargetFolder $targetFolder
