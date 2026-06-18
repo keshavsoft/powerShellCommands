d5c119e828f8dfec7b87b867c3e68d92bc30796d
keshavsoft <nknnkumar@yahoo.com>
2026-06-18 20:10:30 +0530

    t2

---
+++ Build/TallyServer/V2/main.ps1
@@ -0,0 +1,53 @@
+$repos = @(
+    "https://github.com/keshavsoft/tallyServerV1",    
+    
+    "https://github.com/keshavsoft/json-crud-fs",
+    "https://github.com/keshavsoft/json-crud-ui-table",
+    "https://github.com/keshavsoft/json-crud-ui-comp",
+    "https://github.com/keshavsoft/json-crud-ui-comp-table",
+    "https://github.com/keshavsoft/tailwind-table-dom",
+    "https://github.com/keshavsoft/tailwind-header-dom",
+    "https://github.com/keshavsoft/tailwind-table-dom-comp",
+    "https://github.com/keshavsoft/tailwind-vertical-dom",
+    "https://github.com/keshavsoft/tailwind-gen-css",
+    "https://github.com/keshavsoft/vs-code-ext-json-crud-ui-table",
+    "https://github.com/keshavsoft/vs-code-ext-express-api-gen-endpoints",
+    "https://github.com/keshavsoft/EndPointGenWebsite",
+    "https://github.com/keshavsoft/vs-code-ext-express-api-gen-post-actions",
+    "https://github.com/keshavsoft/vs-code-ext-express-api-gen-get-actions",
+    "https://github.com/keshavsoft/kschema-fs-api-gen-actions",
+    "https://github.com/keshavsoft/kschema-fs-api-gen-get-actions",
+    "https://github.com/keshavsoft/kschema-fs-api-gen-post-actions"
+)
+
+function Get-BaseFolder {
+    $inputPath = Read-Host "Workspace path (Press Enter for default)"
+
+    if (-not [string]::IsNullOrWhiteSpace($inputPath)) {
+        return $inputPath
+    }
+
+    if (Test-Path "D:\") {
+        return "D:\KeshavSoftRepos"
+    }
+
+    return (Join-Path $env:USERPROFILE "KeshavSoftRepos")
+}
+
+$baseFolder = Get-BaseFolder
+
+$targetFolder = Get-WorkspaceFolder
+
+Ensure-BaseFolder -inBaseFolder $baseFolder
+
+Ensure-WorkspaceFolder -inTargetFolder $targetFolder
+
+if (-not (Ensure-GitInstalled)) {
+    return
+}
+
+Clone-Repos -inRepos $repos -inTargetFolder $targetFolder
+
+Install-NpmPackages -inTargetFolder $targetFolder
+
+Open-Workspace -inTargetFolder $targetFolder
