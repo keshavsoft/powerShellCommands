a17b270f85d5b59db9d1c1d3bb33b13dd0593210
keshavsoft <nknnkumar@yahoo.com>
2026-07-20 17:16:17 +0530

    patternCollector start

---
+++ Build/patternCollector/routesjs/v1/main.ps1
@@ -0,0 +1,37 @@
+$repos = @(
+    "https://github.com/keshavsoft/pattern-collector",
+    "https://github.com/keshavsoft/pattern-collector-routesjs-use",
+    "https://github.com/keshavsoft/pattern-collector-routesjs-use-extract"
+)
+
+function Get-BaseFolder {
+    $inputPath = Read-Host "Workspace path (Press Enter for default)"
+
+    if (-not [string]::IsNullOrWhiteSpace($inputPath)) {
+        return $inputPath
+    }
+
+    if (Test-Path "D:\") {
+        return "D:\KeshavSoftRepos"
+    }
+
+    return (Join-Path $env:USERPROFILE "KeshavSoftRepos")
+}
+
+$baseFolder = Get-BaseFolder
+
+$targetFolder = Get-WorkspaceFolder
+
+Ensure-BaseFolder -inBaseFolder $baseFolder
+
+Ensure-WorkspaceFolder -inTargetFolder $targetFolder
+
+if (-not (Ensure-GitInstalled)) {
+    return
+}
+
+Clone-Repos -inRepos $repos -inTargetFolder $targetFolder
+
+Install-NpmPackages -inTargetFolder $targetFolder
+
+Open-Workspace -inTargetFolder $targetFolder
