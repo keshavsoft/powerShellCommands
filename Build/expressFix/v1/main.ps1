a1f5ea84664d6e47173de7ce3b8a7f7eb38bcc6d
keshavsoft <nknnkumar@yahoo.com>
2026-07-22 15:39:47 +0530

    expressFixV1

---
+++ Build/expressFix/v1/main.ps1
@@ -0,0 +1,22 @@
+$repos = @(
+    "https://github.com/keshavsoft/express-fix-endPoints-js"
+    "https://github.com/keshavsoft/express-fix-any-js"
+)
+
+$baseFolder = "D:\KeshavSoftRepos"
+
+$targetFolder = Get-WorkspaceFolder
+
+Ensure-BaseFolder -inBaseFolder $baseFolder
+
+Ensure-WorkspaceFolder -inTargetFolder $targetFolder
+
+if (-not (Ensure-GitInstalled)) {
+    return
+}
+
+Clone-Repos -inRepos $repos -inTargetFolder $targetFolder
+
+Install-NpmPackages -inTargetFolder $targetFolder
+
+Open-Workspace -inTargetFolder $targetFolder
