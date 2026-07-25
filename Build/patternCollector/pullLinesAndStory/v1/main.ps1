d3c2105c1028c099e19cf9b10df8fb6127ab7015
keshavsoft <nknnkumar@yahoo.com>
2026-07-25 12:57:10 +0530

    story 1

---
+++ Build/patternCollector/pullLinesAndStory/v1/main.ps1
@@ -0,0 +1,40 @@
+$repos = @(
+    "https://github.com/keshavsoft/pattern-collector-anyjs",
+    "https://github.com/keshavsoft/pattern-collector-anyjs-pull-lines",
+    "https://github.com/keshavsoft/pattern-collector-anyjs-pull-lines-import",
+    "https://github.com/keshavsoft/pattern-collector-anyjs-pull-lines-consumption",
+    "https://github.com/keshavsoft/pattern-collector-anyjs-pull-lines-all",
+    "https://github.com/keshavsoft/pattern-collector-anyjs-build-story"
+)
+
+function Get-BaseFolder {
+    $inputPath = Read-Host "Workspace path (Press Enter for default)"
+
+    if (-not [string]::IsNullOrWhiteSpace($inputPath)) {
+        return $inputPath
+    }
+
+    if (Test-Path "D:\") {
+        return "D:\KeshavSoftRepos"
+    }
+
+    return (Join-Path $env:USERPROFILE "KeshavSoftRepos")
+}
+
+$baseFolder = Get-BaseFolder
+
+$targetFolder = Get-WorkspaceFolder
+
+Ensure-BaseFolder -inBaseFolder $baseFolder
+
+Ensure-WorkspaceFolder -inTargetFolder $targetFolder
+
+if (-not (Ensure-GitInstalled)) {
+    return
+}
+
+Clone-Repos -inRepos $repos -inTargetFolder $targetFolder
+
+Install-NpmPackages -inTargetFolder $targetFolder
+
+Open-Workspace -inTargetFolder $targetFolder
